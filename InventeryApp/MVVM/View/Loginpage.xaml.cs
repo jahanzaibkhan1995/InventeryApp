39a43daa5dcc8fd0798e82efcbb8ffc2ed0693da
jahanzaibkhan1995 <jahanzaibkhan208@gmail.com>
2024-03-06 15:02:42 +0100

    Create Inventery App

---
+++ InventeryApp/MVVM/View/Loginpage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InventeryApp.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for Loginpage.xaml
+    /// </summary>
+    public partial class Loginpage : Window
+    {
+        public Loginpage()
+        {
+            InitializeComponent();
+        }
+        private void Window_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed) 
+            {
+                DragMove();
+            }
+        }
+    }
+}
